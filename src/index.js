54591c419c7f8365defe004807685684846b3264
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-10-06 22:30:18 +0300

    Add styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import  store  from './store/store';
+
 import App from './App';
+
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
